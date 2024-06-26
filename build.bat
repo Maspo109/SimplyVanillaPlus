426186873513d9e4487f711b4177a4557df5dc88
Maspo109 <maspo109+github@gmail.com>
2024-06-26 13:04:29 +0200

    Updated build scripts and added release pipeline

--- build.bat
+++ build.bat
@@ -13,7 +13,9 @@ for /f "tokens=2 delims== " %%a in ('findstr /i "minecraft" pack.toml') do (
     set minecraft=!minecraft: =!
 )
 
-packwiz curseforge export -s server -o "simply-vanilla-plus-mc!minecraft!-!version!-server.zip"
-packwiz curseforge export -s client -o "simply-vanilla-plus-mc!minecraft!-!version!.zip"
+if not exist "build" mkdir "build"
+
+packwiz curseforge export -s server -o "build/simply-vanilla-plus-mc!minecraft!-!version!-server.zip"
+packwiz curseforge export -s client -o "build/simply-vanilla-plus-mc!minecraft!-!version!.zip"
 
 endlocal
--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -e
 version=$(grep -e "^version" pack.toml | cut -f3 -d' ' | tr -d '"')
 minecraft=$(grep -e "^minecraft" pack.toml | cut -f3 -d' ' | tr -d '"')
 
-packwiz curseforge export -s server -o simply-vanilla-plus-mc${minecraft}-${version}-server.zip
-packwiz curseforge export -s client -o simply-vanilla-plus-mc${minecraft}-${version}.zip
+mkdir -p build
+
+packwiz curseforge export -s server -o build/simply-vanilla-plus-mc${minecraft}-${version}-server.zip
+packwiz curseforge export -s client -o build/simply-vanilla-plus-mc${minecraft}-${version}.zip
